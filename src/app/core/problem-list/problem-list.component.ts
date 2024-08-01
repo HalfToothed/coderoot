1bcd074fd9e28907afcee585b33d90d945a544ad
HalfToothed <abhishekandmails@gmail.com>
2024-08-01 20:51:26 +0530

    removed material

--- src/app/core/problem-list/problem-list.component.ts
+++ src/app/core/problem-list/problem-list.component.ts
@@ -1,20 +1,17 @@
-import { Component, ViewChild } from '@angular/core';
-import { MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { Component } from '@angular/core';
 import { ProblemList } from '../../models/problem-list';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
-import { MatSort } from '@angular/material/sort';
 
 const ELEMENT_DATA: ProblemList[] = [
-  {status: true, title: 'Design Dynamic Array (Resizable Array', difficulty: 'Hard', acceptance: 1.0079 },
+  {status: false, title: 'Design Dynamic Array (Resizable Array', difficulty: 'Hard', acceptance: 1.0079 },
   {status: true, title: 'Design Double-ended Queue', difficulty: 'Easy', acceptance: 1.0079 },
-  {status: true, title: 'Design Heap', difficulty: 'Easy', acceptance: 1.0079 },
+  {status: false, title: 'Design Heap', difficulty: 'Easy', acceptance: 1.0079 },
   {status: true, title: 'Strategy Pattern', difficulty: 'Easy', acceptance: 1.0079 },
-  {status: true, title: 'Foreign Dictionary', difficulty: 'Hard', acceptance: 1.0079 },
-  {status: true, title: 'String Encode and Decode', difficulty: 'Easy', acceptance: 1.0079 },
+  {status: false, title: 'Foreign Dictionary', difficulty: 'Hard', acceptance: 1.0079 },
+  {status: false, title: 'String Encode and Decode', difficulty: 'Easy', acceptance: 1.0079 },
   {status: true, title: 'Validate Parentheses', difficulty: 'Medium', acceptance: 1.0079 },
-  {status: true, title: 'Find Median in a Data Stream', difficulty: 'Medium', acceptance: 1.0079 },
+  {status: false, title: 'Find Median in a Data Stream', difficulty: 'Medium', acceptance: 1.0079 },
   {status: true, title: 'Climbing Stairs', difficulty: 'Easy', acceptance: 1.0079 },
-  {status: true, title: 'Longest Repeating Substring With Replacement', difficulty: 'Hard', acceptance: 1.0079 },
+  {status: false, title: 'Longest Repeating Substring With Replacement', difficulty: 'Hard', acceptance: 1.0079 },
   {status: true, title: 'Merge Two Sorted Linked Lists', difficulty: 'Medium', acceptance: 1.0079 },
 ];
 
@@ -22,18 +19,10 @@ const ELEMENT_DATA: ProblemList[] = [
 @Component({
   selector: 'app-problem-list',
   standalone: true,
-  imports: [MatTableModule, MatFormField, MatLabel, MatSort],
+  imports: [],
   templateUrl: './problem-list.component.html',
   styleUrl: './problem-list.component.css'
 })
 export class ProblemListComponent {
 
-  @ViewChild(MatSort) sort: MatSort;
-
-  displayedColumns: string[] = ['status', 'title', 'difficulty', 'acceptance'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
-  
-    ngOnInit() {
-      this.dataSource.sort = this.sort;
-    } 
 }
